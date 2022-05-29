92f1a48316f736df986a0897a9760dade84135b8
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-29 16:18:31 -0400

    Adding ternary operater to render Weather screen

--- src/App.js
+++ src/App.js
@@ -1,45 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import DailyTemp from './components/DailyTemp';
-import SearchInput from './components/SearchInput';
-import {useEffect, useState} from 'react'
-import axios from 'axios';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import DailyTemp from "./components/DailyTemp";
+import SearchInput from "./components/SearchInput";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [coordinates, setCoordinates] = useState([]);
-  
-  useEffect(()=> {
+  const [temperature, setTemperature] = useState([]);
+  const [city, setCity] = useState([]);
 
-  }, [])
+  useEffect(() => {}, []);
 
-
-  const submitPostalCode = async(event) => {
+  const submitPostalCode = async (event) => {
     event.preventDefault();
 
-    const postalCode = event.target[0].value
+    const postalCode = event.target[0].value;
     try {
+      const newGeoCode = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=${process.env.REACT_APP_GEOCODE}`
+      );
 
-      const newGeoCode = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=${process.env.REACT_APP_GEOCODE}`)
-      
-      const {name, lat, lon} = newGeoCode.data
+      const { name, lat, lon } = newGeoCode.data;
 
-      const weatherAtCoordinates = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER}`)
-      console.log(weatherAtCoordinates)
-
-
+      const weatherAtCoordinates = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER}`
+      );
+      setTemperature(weatherAtCoordinates.data.main);
+      setCity(name);
     } catch (error) {
-      console.log("Invalid postal Code")
+      console.log("Invalid postal Code");
     }
-  
-
-  }
+  };
 
   return (
     <div className="App">
-      <SearchInput submitPostalCode={submitPostalCode}/>
-      <DailyTemp />
+      <SearchInput submitPostalCode={submitPostalCode} />
+      <DailyTemp temperature={temperature} city={city}/>
     </div>
   );
 }
--- src/components/DailyTemp.jsx
+++ src/components/DailyTemp.jsx
@@ -1,7 +1,23 @@
-export default function DailyTemp() {
+export default function DailyTemp({city, temperature}) {
+
+  const {temp, feels_like, temp_max, temp_min} = temperature;
+
+  console.log(temperature)
+  const kelvinToCelsius = (kelvin) => {
+    return (kelvin - 273.15).toFixed(2);
+  }
   return(
     <>
       Daily Weather
+
+      {temperature.length === 0  ? <h2>Please enter a postal code</h2> :  
+      <>
+      <h1>{city}</h1>
+      <p>Current Temp: {kelvinToCelsius(temp)}°C</p>
+      <p>Feels Like: {kelvinToCelsius(feels_like)}°C</p>
+      <p>Daily High: {kelvinToCelsius(temp_max)}°C</p>
+      <p>Daily Low: {kelvinToCelsius(temp_min)}°C </p>
+      </>}
     </>
   )
 }
