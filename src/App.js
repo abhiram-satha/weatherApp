3d440c4312cef388036121bd477d5bcee9c62aec
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-29 16:51:33 -0400

    Adding weather background

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import axios from "axios";
 function App() {
   const [temperature, setTemperature] = useState([]);
   const [city, setCity] = useState([]);
+  const [weatherArray, setWeatherArray] = useState([]);
 
   useEffect(() => {}, []);
 
@@ -25,8 +26,10 @@ function App() {
       const weatherAtCoordinates = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER}`
       );
+      console.log(weatherAtCoordinates);
       setTemperature(weatherAtCoordinates.data.main);
       setCity(name);
+      setWeatherArray(weatherAtCoordinates.data.weather[0]);
     } catch (error) {
       console.log("Invalid postal Code");
     }
@@ -35,7 +38,7 @@ function App() {
   return (
     <div className="App">
       <SearchInput submitPostalCode={submitPostalCode} />
-      <DailyTemp temperature={temperature} city={city}/>
+      <DailyTemp temperature={temperature} city={city} weatherArray={weatherArray}/>
     </div>
   );
 }
--- src/components/DailyTemp.jsx
+++ src/components/DailyTemp.jsx
@@ -1,23 +1,27 @@
-export default function DailyTemp({city, temperature}) {
+import "./DailyTemp.css";
+
+export default function DailyTemp({city, temperature, weatherArray}) {
 
   const {temp, feels_like, temp_max, temp_min} = temperature;
+  const {main, description} = weatherArray
+  console.log(weatherArray)
 
-  console.log(temperature)
+
+  // console.log(temperature)
   const kelvinToCelsius = (kelvin) => {
     return (kelvin - 273.15).toFixed(2);
   }
   return(
     <>
-      Daily Weather
 
       {temperature.length === 0  ? <h2>Please enter a postal code</h2> :  
-      <>
+      <div className={main}>
       <h1>{city}</h1>
       <p>Current Temp: {kelvinToCelsius(temp)}°C</p>
       <p>Feels Like: {kelvinToCelsius(feels_like)}°C</p>
       <p>Daily High: {kelvinToCelsius(temp_max)}°C</p>
       <p>Daily Low: {kelvinToCelsius(temp_min)}°C </p>
-      </>}
+      </div>}
     </>
   )
 }
