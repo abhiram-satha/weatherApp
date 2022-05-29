bc2c637873a511cceb0cdb55bdc92ab93daaa6f2
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-29 15:28:19 -0400

    Moving api to env file

--- src/App.js
+++ src/App.js
@@ -21,10 +21,14 @@ function App() {
     const postalCode = event.target[0].value
     try {
 
-      const newGeoCode = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=ef00555378c1b7ea22939088241c7dc8`)
+      const newGeoCode = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=${process.env.REACT_APP_GEOCODE}`)
       
       const {name, lat, lon} = newGeoCode.data
-      console.log(name)
+
+      const weatherAtCoordinates = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER}`)
+      console.log(weatherAtCoordinates)
+
+
     } catch (error) {
       console.log("Invalid postal Code")
     }
