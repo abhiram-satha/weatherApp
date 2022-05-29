a38a949bec603e60135ddb5da71d8ee753921212
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-05-29 13:21:08 -0400

    Creating try/catch for error handling

--- src/App.js
+++ src/App.js
@@ -17,11 +17,17 @@ function App() {
 
   const submitPostalCode = async(event) => {
     event.preventDefault();
-    const postalCode = event.target[0].value
 
-    const newGeoCode = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=ef00555378c1b7ea22939088241c7dc8`)
-    
-    console.log(newGeoCode.data)
+    const postalCode = event.target[0].value
+    try {
+
+      const newGeoCode = await axios.get(`http://api.openweathermap.org/geo/1.0/zip?zip=${postalCode},CA&appid=ef00555378c1b7ea22939088241c7dc8`)
+      
+      const {name, lat, lon} = newGeoCode.data
+      console.log(name)
+    } catch (error) {
+      console.log("Invalid postal Code")
+    }
   
 
   }
