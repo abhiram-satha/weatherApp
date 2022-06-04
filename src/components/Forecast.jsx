a94cb1f164f0b2b80d5ca3f113c91ecc52d8848e
Abhiram Sathasivam <abhiram.satha@gmail.com>
2022-06-03 20:37:42 -0400

    Rendering month for weekly forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -11,9 +11,36 @@ export default function Forecast({
   const monthCalculator = () => {
     if(dt_txt[5] === "1") {
       if(dt_txt[6] === "0") {
-        return "Oct"
+        return "Oct "
+      } else if (dt_txt[6] === "1") {
+        return "Nov "
+      } else {
+        return "Dec "
+      }
+    } else {
+      if(dt_txt[6] === "1") {
+        return "Jan "
+      } else if (dt_txt[6] === "2") {
+        return "Feb "
+      } else if (dt_txt[6] === "3") {
+        return "Mar "
+      } else if (dt_txt[6] === "4") {
+        return "Apr "
+      } else if (dt_txt[6] === "5") {
+        return "May "
+      } else if (dt_txt[6] === "6") {
+        return "Jun "
+      } else if (dt_txt[6] === "7") {
+        return "Jul "
+      } else if (dt_txt[6] === "8") {
+        return "Aug "
+      } else if (dt_txt[6] === "9") {
+        return "Sep "
+      }else {
+        return ""
       }
     }
+
     
   }
 
@@ -24,7 +51,8 @@ export default function Forecast({
     <div className={weather[0].main}>
       <div className="forecast-flex">
         <h5>
-          {dt_txt[8]}
+          {monthCalculator()}
+           {dt_txt[8]}
           {dt_txt[9]}
         </h5>
         {standardTemp ? (
